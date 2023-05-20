b42ca4492590c6c88c7fefa1512e1b2441d6bf34
cc004 <1176321897@qq.com>
2023-05-20 20:34:30 +0800

    make timeopen cfg available for http server only mode

--- autopcr/module/crons.py
+++ autopcr/module/crons.py
@@ -4,16 +4,17 @@ import os
 from ..module.modulebase import ModuleManager
 
 async def _cron(task):
-    hour = datetime.datetime.now().hour
+    hour = None
     while True:
-        await asyncio.sleep(60)
         if datetime.datetime.now().hour != hour:
             await task(hour)
+        await asyncio.sleep(60)
 
 async def _run_crons(path, hour):
     for file in os.listdir(path):
         if file.endswith('.json'):
             mgr = ModuleManager(os.path.join(path, file))
+            print(f'Running cron for {file}, crons = {mgr._crons}')
             await mgr.do_cron(hour)
 
 def queue_crons(path):
