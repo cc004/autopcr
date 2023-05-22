dcc42fc423e15521b6bd354732175ef57ae0dd5a
cc004 <1176321897@qq.com>
2023-05-22 21:47:31 +0800

    Update apiclient.py

--- autopcr/core/apiclient.py
+++ autopcr/core/apiclient.py
@@ -151,10 +151,10 @@ class apiclient(Container["apiclient"]):
         #     fp.write(json.dumps(response0))
         response: Response[TResponse] = Response[cls].parse_obj(response0)
         
-        with open('req.log', 'a') as fp:
-            fp.write(f'{self.name} requested {request.__class__.__name__} at /{request.url}\n')
-            fp.write(json.dumps(json.loads(request.json(by_alias=True)), indent=4, ensure_ascii=False) + '\n')
-            fp.write(json.dumps(json.loads(response.json(by_alias=True)), indent=4, ensure_ascii=False) + '\n')
+        #with open('req.log', 'a') as fp:
+        #    fp.write(f'{self.name} requested {request.__class__.__name__} at /{request.url}\n')
+        #    fp.write(json.dumps(json.loads(request.json(by_alias=True)), indent=4, ensure_ascii=False) + '\n')
+        #    fp.write(json.dumps(json.loads(response.json(by_alias=True)), indent=4, ensure_ascii=False) + '\n')
         
         if response.data_headers.servertime:
             self.server_time = response.data_headers.servertime
