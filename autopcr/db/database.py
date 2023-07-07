431f84a97ee5340f6f12d15448c6efe96765127b
Lanly109 <1094916227@qq.com>
2023-07-07 11:51:02 +0800

    fix title name unknown

--- autopcr/db/database.py
+++ autopcr/db/database.py
@@ -266,6 +266,10 @@ class database():
                     RoomItem.query(db)
                     .select(lambda x: (eInventoryType.RoomItem, x.id, x.name))
                 )
+                .concat(
+                    EmblemDatum.query(db)
+                    .select(lambda x: (x.type, x.emblem_id, x.emblem_name)
+                )
                 .to_dict(lambda x: (x[0], x[1]), lambda x: x[2])
             )
 
