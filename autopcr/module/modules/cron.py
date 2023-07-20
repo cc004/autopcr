cc3dc21853e97ef23abd329ebab21f262a31c253
Lanly109 <1094916227@qq.com>
2023-07-20 16:04:58 +0800

    update server draw

--- autopcr/module/modules/cron.py
+++ autopcr/module/modules/cron.py
@@ -4,7 +4,8 @@ from ...core.pcrclient import pcrclient
 from ...model.error import *
 from ...model.enums import *
 
-@timetype("time1", "执行时间", "00:00")
+@timetype("time_cron1", "执行时间", "00:00")
+@booltype("clanbattle_run_cron1", "会战期间执行", False)
 @description('定时执行')
 @name("定时任务1")
 @default(False)
