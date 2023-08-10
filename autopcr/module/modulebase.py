56440ac071e1244366c749cf2b3e460bbce673e8
Lanly109 <1094916227@qq.com>
2023-08-11 00:59:32 +0800

    fix valid config check in list candidates

--- autopcr/module/modulebase.py
+++ autopcr/module/modulebase.py
@@ -81,7 +81,10 @@ class Module:
         else:
             default = self.config[key].default
         value = self._parent.get_config(key, default)
-        if key != self.key and self.config[key].candidates and value not in self.config[key].candidates:
+        if key != self.key and self.config[key].candidates and (
+            value not in self.config[key].candidates and
+            any(item not in self.config[key].candidates for item in value)
+            ):
             value = default
         return value
 
