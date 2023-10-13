17662ef956653fa7d43f31e500a7d1d6de44f402
SUN LI <51525400+cca2878@users.noreply.github.com>
2023-10-13 17:53:52 +0800

    Update game version
    改成996一劳永逸

--- autopcr/constants.py
+++ autopcr/constants.py
@@ -6,7 +6,7 @@ DATA_DIR = os.path.join(ROOT_DIR, './data/')
 CONFIG_PATH = os.path.join(ROOT_DIR, 'autopcr/http_server/config') # 兼容旧版
 
 
-VERSION = "6.2.0"
+VERSION = "9.9.6"
 
 DEFAULT_HEADERS = {
     'Accept-Encoding': 'gzip',
