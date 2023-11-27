0e7ff5e86174a5c74c2036aa3bdb76a6185b26c9
cc004 <1176321897@qq.com>
2023-11-27 16:23:48 +0800

    split nb from task

--- _task.py
+++ _task.py
@@ -1,18 +1,23 @@
-from abc import abstractclassmethod, abstractproperty
-from nonebot import MessageSegment
-from hoshino.typing import MessageSegment
+from abc import abstractclassmethod, abstractmethod, abstractproperty
 import traceback
 
 from .autopcr.module.accountmgr import instance as accountmgr
-from ._util import draw, render_forward_msg
+from ._util import draw
 
+class callback:
+    @abstractmethod
+    async def send(self, msg: str = '', img: str = ''): ...
+
+    @abstractmethod
+    async def request_validate(self, url: str): ...
+
+    @abstractproperty
+    def qid(self): ...
 
 class Task():
-    def __init__(self, token, bot, ev, qid=None, gid=None, config={}):
-        alian, target = token
-        if qid is None and ev is not None:
-            qid = ev.user_id
-        self.info = (alian, target, bot, ev, qid, gid)
+    def __init__(self, token, callback: callback, config={}):
+        _, target = token
+        self.callback = callback
         self.config = config
         self.token = token
         self.username = accountmgr.load(target, readonly=True).username
@@ -28,15 +33,15 @@ class TaskList(Task):
     def do_module_list(self) -> list: ...
 
     async def do_task(self):
-        alian, target, bot, ev, qid, gid = self.info
+        alian, target = self.token
         try:
             async with accountmgr.load(target) as mgr:
                 resp = await mgr.do_from_key(self.config, self.do_module_list())
-            img = await draw(resp, alian + '_' + '_'.join(self.do_module_list()), qid)
-            await bot.send(ev, f"[CQ:reply,id={ev.message_id}]" + MessageSegment.image(f'file:///{img}'))
+            img = await draw(resp, alian + '_' + '_'.join(self.do_module_list()), self.callback.qid)
+            self.callback.send(img = img)
         except Exception as e:
             traceback.print_exc()
-            await bot.send(ev, f"[CQ:reply,id={ev.message_id}]" + str(e))
+            self.callback.send(msg = str(e))
 
 class ClanBattleSupport(TaskList):
     def do_module_list(self) -> list:
@@ -76,26 +81,16 @@ class Gacha(TaskList):
 
 class DailyClean(Task):
     async def do_task(self):
-        alian, target, bot, ev, qid, gid = self.info
+        alian, target = self.token
         try:
-            if ev:
-                await bot.send(ev, f"[CQ:reply,id={ev.message_id}]开始为{alian}清理日常")
-            else:
-                await bot.send_group_msg(group_id=gid, message=f"【定时任务】开始为{alian}清理日常")
+            await self.callback.send(msg = f"开始为{alian}清理日常")
         except Exception as e:  # 风控，怕report_to_su还失败，就不整了
             print(e)
 
         try:
             async with accountmgr.load(target) as mgr:
                 resp = await mgr.do_daily()
-            img = await draw(resp, alian, qid)
-            if ev:
-                await bot.send(ev, f"[CQ:reply,id={ev.message_id}] {alian}" + MessageSegment.image(f'file:///{img}'))
-            else:
-                await bot.send_group_msg(group_id=gid,
-                                         message=f"【定时任务】{alian}" + MessageSegment.image(f'file:///{img}'))
+            img = await draw(resp, alian, self.callback.qid)
+            await self.callback.send(msg = alian, img = img)
         except Exception as e:
-            if ev:
-                await bot.send(ev, f"[CQ:reply,id={ev.message_id}] {alian}:" + str(e))
-            else:
-                await bot.send_group_msg(group_id=gid, message=f"【定时任务】{alian}:" + str(e))
+            await self.callback.send(msg = f'{alian}: {e}')
