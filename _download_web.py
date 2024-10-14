71ee23e9a824f1d93c39ab8d8792e5f2b9e82ac3
Lanly109 <1094916227@qq.com>
2024-10-14 14:40:27 +0800

    fea: manual web install

--- _download_web.py
+++ _download_web.py
@@ -1,22 +1,22 @@
 import asyncio
 import aiohttp
-from requests import get
 import os
 import shutil
 
 async def get_latest_release_info(owner, repo):
     url = f"https://api.github.com/repos/{owner}/{repo}/releases/latest"
-    response = get(url)
-    if response.ok:
-        release_info = response.json()
-        tag_name = release_info['tag_name']
-        assets = release_info['assets']
-        asset_download_urls = [asset['browser_download_url'] for asset in assets]
-        return tag_name, asset_download_urls
-    else:
-        print(response.status_code)
-        print(response.json())
-        return None, None
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if response.ok:
+                release_info = await response.json()
+                tag_name = release_info['tag_name']
+                assets = release_info['assets']
+                asset_download_urls = [asset['browser_download_url'] for asset in assets]
+                return tag_name, asset_download_urls
+            else:
+                print(response.status)
+                print(await response.json())
+                return None, None
 
 path = os.path.dirname(os.path.abspath(__file__))
 
@@ -37,37 +37,42 @@ async def save_version(version):
         f.write(version)
 
 async def download_assets(asset_download_urls):
-    web_path = os.path.join(path, "autopcr", "http_server", "ClientApp")
-    if not os.path.exists(web_path):
-        os.makedirs(web_path)
+    ret = []
     async with aiohttp.ClientSession() as session:
         for url in asset_download_urls:
             async with session.get(url) as response:
                 if response.status == 200:
                     filename = url.split('/')[-1]
-                    print(f"Downloading {filename}")
                     filepath = os.path.join(path, filename)
+                    print(f"Downloading {filename} -> {filepath}")
                     with open(filepath, 'wb') as f:
                         while True:
                             chunk = await response.content.read(1024)
                             if not chunk:
                                 break
                             f.write(chunk)
-                    print(f"Downloaded {filename}")
-
-                    shutil.rmtree(web_path)
-                    print(f"Removed old web file")
-
-                    import zipfile
-                    with zipfile.ZipFile(filepath, 'r') as zip_ref:
-                        zip_ref.extractall(web_path)
-                    print(f"Unzipped {filename}")
-
-                    os.remove(filepath)
-                    print(f"Delete {filename}")
+                    ret.append(filepath)
                 else:
                     print(f"Failed to download {url}")
                     raise Exception(f"Failed to download {url}")
+    return ret
+
+async def extract_web(filepaths):
+    web_path = os.path.join(path, "autopcr", "http_server", "ClientApp")
+    if not os.path.exists(web_path):
+        os.makedirs(web_path)
+
+    print(f"Removed old web file")
+    shutil.rmtree(web_path)
+
+    for filepath in filepaths:
+        print(f"Unzipped {filepath}")
+        import zipfile
+        with zipfile.ZipFile(filepath, 'r') as zip_ref:
+            zip_ref.extractall(web_path)
+
+        print(f"Delete {filepath}")
+        os.remove(filepath)
 
 async def do_download():
     owner = 'Lanly109'
@@ -76,7 +81,8 @@ async def do_download():
     if latest_release_tag and asset_download_urls:
         print(f"Latest release tag: {latest_release_tag}")
         if await check_version(latest_release_tag):
-            await download_assets(asset_download_urls)
+            filepaths = await download_assets(asset_download_urls)
+            await extract_web(filepaths)
             await save_version(latest_release_tag)
         else:
             print("Already up to date")
@@ -84,6 +90,15 @@ async def do_download():
         print("Failed to fetch latest release info")
         raise Exception("Failed to fetch latest release info")
 
+async def main(zips):
+    if zips:
+        await extract_web(zips)
+    else:
+        await do_download()
+
+
 if __name__ == "__main__":
+    import sys
+    zips = sys.argv[1:]
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(do_download())
+    loop.run_until_complete(main(zips))
