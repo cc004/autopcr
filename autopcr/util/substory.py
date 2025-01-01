4a57bdc33315cfcf825fb42b22faa97e3b189689
lclbm <614867321@qq.com>
2025-01-01 14:41:24 +0800

    feat: dsb substory reader

--- autopcr/util/substory.py
+++ autopcr/util/substory.py
@@ -32,6 +32,15 @@ def GetSubStoryReader(sub_story_data: EventSubStory, client: pcrclient) -> Union
         return constructor[sub_story_data.event_id](client)
     return None
 
+@EventId(10108)
+class dsb_substory(SubStoryReader):
+
+    def title(self, sub_story_id: int) -> str:
+        return db.dsb_story_data[sub_story_id].title
+
+    async def read(self, sub_story_id: int):
+        await self.client.read_dsb_story(sub_story_id)
+
 @EventId(10106)
 class xeh_substory(SubStoryReader):
 
