b542890c7761018dd54964f118d17018dd80c9cc
Lanly109 <1094916227@qq.com>
2025-03-15 15:18:53 +0800

    fea: support byway story reading

--- autopcr/model/handlers.py
+++ autopcr/model/handlers.py
@@ -333,6 +333,8 @@ class LoadIndexResponse(responses.LoadIndexResponse):
 class HomeIndexResponse(responses.HomeIndexResponse):
     async def update(self, mgr: datamgr, request):
         mgr.finishedQuest |= set(([q.quest_id for q in self.quest_list if q.result_type > 0 and q.clear_flg == 3] if self.quest_list else []) + ([q.quest_id for q in self.shiori_quest_info.quest_list if q.result_type > 0 and q.clear_flg == 3] if self.shiori_quest_info and self.shiori_quest_info.quest_list else []))
+        if self.cleared_byway_quest_id_list:
+            mgr.cleared_byway_quest_id_set |= set(self.cleared_byway_quest_id_list)
         if self.user_clan:
             mgr.clan = self.user_clan.clan_id
         if self.user_clan and self.user_clan.donation_num:
