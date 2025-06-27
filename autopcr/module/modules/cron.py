05ace2116388631169bd3b33b61958c5e2e7c1bd
Lanly109 <1094916227@qq.com>
2025-06-27 18:58:40 +0800

    fea: update cron

--- autopcr/module/modules/cron.py
+++ autopcr/module/modules/cron.py
@@ -27,15 +27,25 @@ class CronModule(Module):
 
 class NormalCronModule(CronModule):
     def get_clanbattle_run_status(self) -> bool: ...
+    def get_module_exclude_type(self) -> bool: ...
 
     async def is_cron_condition(self) -> bool:
         is_clan_battle_run = self.get_clanbattle_run_status()
         is_clan_battle_time = db.is_clan_battle_time()
         return not is_clan_battle_time or is_clan_battle_run
 
+    async def update_client(self, client: pcrclient):
+        await super().update_client(client)
+        module_exclude_type = self.get_module_exclude_type()
+        if module_exclude_type == '体力获取':
+            client.set_stamina_get_not_run()
+        elif module_exclude_type == '体力消耗':
+            client.set_stamina_consume_not_run()
 
-@timetype("time_cron1", "执行时间", "00:00")
+
+@singlechoice("module_exclude_type_cron1", "不执行日常模块", '无', ['无', '体力获取', '体力消耗'])
 @booltype("clanbattle_run_cron1", "会战期间执行", False)
+@timetype("time_cron1", "执行时间", "00:00")
 @description('定时执行')
 @name("定时任务1")
 @default(False)
@@ -45,9 +55,12 @@ class cron1(NormalCronModule):
         return self.get_config("time_cron1")
     def get_clanbattle_run_status(self) -> bool:
         return self.get_config("clanbattle_run_cron1")
+    def get_module_exclude_type(self) -> str:
+        return self.get_config("module_exclude_type_cron1")
 
-@timetype("time_cron2", "执行时间", "00:00")
+@singlechoice("module_exclude_type_cron2", "不执行日常模块", '无', ['无', '体力获取', '体力消耗'])
 @booltype("clanbattle_run_cron2", "会战期间执行", False)
+@timetype("time_cron2", "执行时间", "00:00")
 @description('定时执行')
 @name("定时任务2")
 @default(False)
@@ -57,10 +70,13 @@ class cron2(NormalCronModule):
         return self.get_config("time_cron2")
     def get_clanbattle_run_status(self) -> bool:
         return self.get_config("clanbattle_run_cron2")
+    def get_module_exclude_type(self) -> str:
+        return self.get_config("module_exclude_type_cron2")
 
 
-@timetype("time_cron3", "执行时间", "00:00")
+@singlechoice("module_exclude_type_cron3", "不执行日常模块", '无', ['无', '体力获取', '体力消耗'])
 @booltype("clanbattle_run_cron3", "会战期间执行", False)
+@timetype("time_cron3", "执行时间", "00:00")
 @description('定时执行')
 @name("定时任务3")
 @default(False)
@@ -70,9 +86,12 @@ class cron3(NormalCronModule):
         return self.get_config("time_cron3")
     def get_clanbattle_run_status(self) -> bool:
         return self.get_config("clanbattle_run_cron3")
+    def get_module_exclude_type(self) -> str:
+        return self.get_config("module_exclude_type_cron3")
 
-@timetype("time_cron4", "执行时间", "00:00")
+@singlechoice("module_exclude_type_cron4", "不执行日常模块", '无', ['无', '体力获取', '体力消耗'])
 @booltype("clanbattle_run_cron4", "会战期间执行", False)
+@timetype("time_cron4", "执行时间", "00:00")
 @description('定时执行')
 @name("定时任务4")
 @default(False)
@@ -82,12 +101,14 @@ class cron4(NormalCronModule):
         return self.get_config("time_cron4")
     def get_clanbattle_run_status(self) -> bool:
         return self.get_config("clanbattle_run_cron4")
+    def get_module_exclude_type(self) -> str:
+        return self.get_config("module_exclude_type_cron4")
 
+@conditional_execution2('run_condition_cron5', [], desc='执行庆典', check=False)
 @timetype("time_cron5", "执行时间", "00:00")
 @description('该定时任务不执行体力消耗的操作')
 @name("特别定时任务1")
 @default(False)
-@conditional_execution2('run_condition_cron5', [], desc='执行庆典', check=False)
 @notrunnable
 class cron5(CronModule):
     def get_cron_time(self) -> str:
@@ -102,11 +123,11 @@ class cron5(CronModule):
         await super().update_client(client)
         client.set_stamina_consume_not_run()
 
+@conditional_execution2('run_condition_cron6', [], desc='执行庆典', check=False)
 @timetype("time_cron6", "执行时间", "00:00")
 @description('该定时任务不执行体力获取的操作')
 @name("特别定时任务2")
 @default(False)
-@conditional_execution2('run_condition_cron6', [], desc='执行庆典', check=False)
 @notrunnable
 class cron6(CronModule):
     def get_cron_time(self) -> str:
