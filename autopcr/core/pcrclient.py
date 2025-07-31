c9b54d02b5b966a7d7a4d59116e7994eac9ff61d
Lanly109 <1094916227@qq.com>
2025-07-31 22:23:02 +0800

    fea: support asb sub story

--- autopcr/core/pcrclient.py
+++ autopcr/core/pcrclient.py
@@ -609,6 +609,11 @@ class pcrclient(apiclient):
         await self.story_check(story_id)
         return await self.story_view(story_id)
 
+    async def read_asb_story(self, sub_story_id: int):
+        req = SubStoryAsbReadStoryRequest()
+        req.sub_story_id = sub_story_id
+        await self.request(req)
+
     async def read_wtm_story(self, sub_story_id: int):
         req = SubStoryWtmReadStoryRequest()
         req.sub_story_id = sub_story_id
--- autopcr/util/substory.py
+++ autopcr/util/substory.py
@@ -35,6 +35,15 @@ def GetSubStoryReader(sub_story_data: EventSubStory, client: pcrclient) -> Union
         return constructor[sub_story_data.event_id](client)
     return None
 
+@EventId(10130)
+class asb_substory(SubStoryReader):
+
+    def title(self, sub_story_id: int) -> str:
+        return db.asb_story_data[sub_story_id].title
+
+    async def read(self, sub_story_id: int):
+        await self.client.read_asb_story(sub_story_id)
+
 @EventId(10128)
 class wtm_substory(SubStoryReader):
 
