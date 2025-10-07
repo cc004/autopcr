7bd61ee3aa66d5a6b1f83e4d8bb06ac8155fc5ec
Lanly109 <1094916227@qq.com>
2025-10-07 21:13:38 +0800

    fea: move master_shop from tools to daily

--- autopcr/module/modules/__init__.py
+++ autopcr/module/modules/__init__.py
@@ -113,6 +113,7 @@ daily_modules = ModuleList(
         pjjc_shop,
         clanbattle_shop,
         master_shop_talent,
+        master_shop,
 
         clan_equip_request,
         love_up,
@@ -159,7 +160,6 @@ unit_modules = ModuleList(
     'unit',
     [
         search_unit,
-        master_shop,
         missing_unit,
         refresh_box,
         unit_promote,
