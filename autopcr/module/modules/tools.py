21502e7b6b2c0b18e411f463a6a9ee039c0aea23
Lanly109 <1094916227@qq.com>
2025-11-08 18:22:45 +0800

    fix: clear team number

--- autopcr/module/modules/tools.py
+++ autopcr/module/modules/tools.py
@@ -593,8 +593,8 @@ class get_normal_quest_recommand(Module):
 #         self._log(msg)
 
 @description('从指定面板的指定队开始清除指定数量的编队')
-@inttype("clear_team_num", "队伍数", 1, [i for i in range(1, 11)])
-@inttype("clear_party_start_num", "初始队伍", 1, [i for i in range(1, 11)])
+@inttype("clear_team_num", "队伍数", 1, [i for i in range(1, 21)])
+@inttype("clear_party_start_num", "初始队伍", 1, [i for i in range(1, 21)])
 @inttype("clear_tab_start_num", "初始面板", 1, [i for i in range(1, 7)])
 @name('清除编队')
 class clear_my_party(Module):
@@ -605,7 +605,7 @@ class clear_my_party(Module):
         for _ in range(number):
 
             party_number += 1
-            if party_number == 11:
+            if party_number == 21:
                 tab_number += 1
                 party_number = 1
                 if tab_number >= 6:
