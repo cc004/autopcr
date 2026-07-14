68c84b29eb39162302c56340feeb912d0b7007d8
SonderXiaoming <1316864423@qq.com>
2026-07-14 17:44:25 -0400

    Disable EX state module by default

--- autopcr/module/modules/__init__.py
+++ autopcr/module/modules/__init__.py
@@ -219,6 +219,7 @@ tool_modules = ModuleList(
         # cook_pudding,
         ex_equip_rank_up,
         ex_equip_enhance_up,
+        ex_equip_state,
         half_schedule,
         caravan_play,
         caravan_shop_buy,
