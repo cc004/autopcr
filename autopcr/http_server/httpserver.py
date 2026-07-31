0d41570067c7f29ef44fcd5880a90013a1e1616c
Lanly109 <1094916227@qq.com>
2026-07-31 22:41:04 +0800

    fea: update http server version

--- autopcr/http_server/httpserver.py
+++ autopcr/http_server/httpserver.py
@@ -19,7 +19,7 @@ from ..util.draw import instance as drawer
 from ..util.logger import instance as logger
 
 APP_VERSION_MAJOR = 1
-APP_VERSION_MINOR = 7
+APP_VERSION_MINOR = 8
 
 CACHE_HTTP_DIR = os.path.join(CACHE_DIR, 'http_server')
 
