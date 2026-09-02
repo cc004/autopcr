d22756a54fa7ace723d20957baf314dddbf67f43
Flysky701 <18458871liu@gmail.com>
2026-09-02 16:50:47 +0800

    修复: 调整注册限流时机

--- autopcr/module/accountmgr.py
+++ autopcr/module/accountmgr.py
@@ -501,11 +501,14 @@ class UserManager:
     def qid_path(self, qid: str) -> str:
         return os.path.join(self.root, qid)
 
-    def create(self, qid: str, password: str) -> AccountManager:
+    def validate_create(self, qid: str):
         if not UserManager.pathsyntax.fullmatch(qid):
             raise UserException('无效的QQ号')
         if qid in self.qids():
             raise UserException('QQ号已存在')
+
+    def create(self, qid: str, password: str) -> AccountManager:
+        self.validate_create(qid)
         os.makedirs(self.qid_path(qid))
         with open(self.qid_path(qid) + '/secret', 'w') as f:
             f.write(UserData(password=password).to_json())
